7d205ff945e270ee01eeebcf55c3a4f8ab73b659
lucy-cl <cl19940816@163.com>
2021-08-24 20:11:28 +0800

    feat: add i18n

--- .umirc.ts
+++ .umirc.ts
@@ -17,5 +17,10 @@ export default defineConfig({
       ],
     },
   ],
+  locale: {
+    default: 'zh-CN', //默认语言 zh-CN
+    baseNavigator: true, // 为true时，用navigator.language的值作为默认语言
+    antd: true, // 是否启用antd的<LocaleProvider />
+  },
   fastRefresh: {},
 });

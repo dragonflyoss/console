19403df9474472e0e608e96e6dfaf3026f743c5e
jackHU1998 <92803130+jackHU1998@users.noreply.github.com>
2022-12-14 20:43:03 +0800

    init:console
    Signed-off-by: jackHU1998 <92803130+jackHU1998@users.noreply.github.com>

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);

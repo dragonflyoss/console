91d2c450b755aa91b2fe3633b0e11c0048368552
jackHU1998 <92803130+jackHU1998@users.noreply.github.com>
2023-01-18 13:28:30 +0800

    feat: init console (#109)
    Signed-off-by: jackHU1998 <92803130+jackHU1998@users.noreply.github.com>
    Signed-off-by: zhaoxinxin <zxx1186037180@163.com>

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+// Add more setup options before each test is run.
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    'pages/**/*.{ts,tsx}',
+    'pages.test/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+  ],
+  moduleNameMapper: {
+    // Handle module aliases.
+    '^@/src/(.*)$': '<rootDir>/src/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  swcMinify: true,
+});

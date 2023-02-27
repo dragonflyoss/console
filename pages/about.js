dd03bf3319a18ad34681bd9d8d677e959f748ab3
zhaoxinxin <zxx1186037180@163.com>
2023-02-27 22:24:03 +0800

    chore:add pages initial directory
    Signed-off-by: zhaoxinxin <zxx1186037180@163.com>

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+function Ablut() {
+    return (<div>
+      <footer>Footer</footer>
+    </div>);
+}
+exports["default"] = Ablut;

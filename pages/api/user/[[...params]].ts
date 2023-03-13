5f54e092909296978931995cca882c1069b91bd4
zhaoxinxin <zxx1186037180@163.com>
2023-03-13 12:36:11 +0800

    chore:add pages initial directory
    Signed-off-by: zhaoxinxin <zxx1186037180@163.com>

--- pages/api/user/[[...params]].ts
+++ pages/api/user/[[...params]].ts
@@ -1,15 +1,15 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import {URL} from '../../../utils/constans'
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    try {
-        const {query} = req
-      const result = await fetch(`${URL}api/v1/users/signin`,{
-        method:'POST',
-        body:JSON.stringify(query)
-      })
-      const data = await result.json()
-      res.status(200).json({ data })
-    } catch (err) {
-      res.status(500).json({ error: {fetch} })
-    }
+import { NextApiRequest, NextApiResponse } from 'next';
+import { URL } from '../../../utils/constans';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { query } = req;
+    const result = await fetch(`${URL}api/v1/users/signin`, {
+      method: 'POST',
+      body: JSON.stringify(query),
+    });
+    const data = await result.json();
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ error: { fetch } });
   }
+}
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -7,92 +7,83 @@ import SwipeableViews from 'react-swipeable-views';
 import React from 'react';
 import styles from './login.module.css';
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-    '/images/homeimge.png',
+    label: 'homeimge',
+    imgPath: '/images/homepage.png',
   },
   {
-    label: 'Bird',
-    imgPath:
-    '/images/homeimge.png',
+    label: 'Featuresimge',
+    imgPath: '/images/features.png',
   },
   {
-    label: 'Bali, Indonesia',
-    imgPath:
-    '/images/homeimge.png',
+    label: 'Milestonesimge',
+    imgPath: '/images/homepage.png',
   },
-  
 ];
-
-
- 
 export default function Login() {
   const [cond, setCond] = useState(true);
   const theme = useTheme();
   const [imageList, setImageList] = useState(images);
-  const [imageIndex, setImageIndex] = useState(0)
-
-  // const [activeStep, setActiveStep] = React.useState(0)
-
-
+  const [imageIndex, setImageIndex] = useState(0);
   const handleStepChange = (step: number) => {
     // setActiveStep(step);
     setImageIndex(step);
   };
   const getcount = () => {
     setCond(false);
-
   };
   const signup = () => {
     setCond(true);
   };
-  const handleChangeIndex=(event: any, index: number)=>{
-   
-    
-    setImageIndex(index -1 );
-  
-    // setActiveStep(activeStep)
-  }
-  
-  
-  
+  const handleChangeIndex = (event: any, index: number) => {
+    setImageIndex(index - 1);
+  };
   return (
-    <div>
-      <Grid container>
-        <Grid item xs={6}>
+    <Grid
+      container
+      sx={{
+        overflowY: 'hidden',
+      }}
+    >
+      <Grid item xs={6}>
         <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={imageIndex}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {imageList.map((step, index) => {
-          return (
-            <div key={step.label}>
-            {Math.abs(imageIndex - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{   
-                
-                  width: '100%',
-                }}
-                src={step.imgPath}
-              />
-            ) : null}
-          </div>
-          );
-        })}
-      </AutoPlaySwipeableViews>
-      <Pagination className={styles.Pagination} page={imageIndex + 1} count={imageList?.length} onChange={handleChangeIndex} hidePrevButton  />
-        </Grid>
-        <Grid item xs={6}>
-          {cond ? <Signin onGetcount={getcount }></Signin> : <SignUp onSetgnup={signup}></SignUp>}
-        </Grid>
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={imageIndex}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {imageList.map((step, index) => {
+            return (
+              <div key={step.label}>
+                {Math.abs(imageIndex - index) <= 2 ? (
+                  <Box
+                    component="img"
+                    sx={{
+                      width: '100%',
+                      height: '100%',
+                    }}
+                    src={step.imgPath}
+                  />
+                ) : null}
+              </div>
+            );
+          })}
+        </AutoPlaySwipeableViews>
+        <Pagination
+          className={styles.Pagination}
+          page={imageIndex + 1}
+          count={imageList?.length}
+          onChange={handleChangeIndex}
+          hidePrevButton
+          hideNextButton
+          color="primary"
+        />
       </Grid>
-    </div>
+      <Grid item xs={6}>
+        {cond ? <Signin onGetcount={getcount}></Signin> : <SignUp onSetgnup={signup}></SignUp>}
+      </Grid>
+    </Grid>
   );
 }
 // export async function getStaticProps() {

1739e0a35d3d94547b6ac2b6939b54652bb1a346
zhaoxinxin <zxx1186037180@163.com>
2023-03-29 10:44:55 +0800

    chore:add pages initial directory
    Signed-off-by: zhaoxinxin <zxx1186037180@163.com>

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import { URL } from 'next/dist/compiled/@edge-runtime/primitives/url';
 
 const API_URL = process.env.NEXT_PUBLIC_DRAGONFLY_PUBLIC_API;
 
-export async function get(url: string | URL) {
+export async function get(url: URL) {
   try {
     const data = await fetch(url).then((res) => res.json());
     return data;
@@ -14,7 +14,7 @@ export async function get(url: string | URL) {
   }
 }
 
-export async function post(url: string | URL, data: any) {
+export async function post(url: URL, data: any) {
   try {
     const response = await fetch(url, {
       method: 'POST',
@@ -38,6 +38,7 @@ interface signinData {
   name: string;
   password: string;
 }
+
 export async function signIn(data: signinData) {
   const url = new URL('/api/v1/users/signin', API_URL);
   return await post(url, data);
@@ -48,6 +49,7 @@ interface signupData {
   password: string;
   email: string;
 }
+
 export async function signUp(data: signupData) {
   const url = new URL('/api/v1/users/signup', API_URL);
   return await post(url, data);

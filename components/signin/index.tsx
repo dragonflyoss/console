d085208fef43b88094548f4420523856597aa89d
zhaoxinxin <107842350+1zhaoxinxin@users.noreply.github.com>
2023-08-01 18:18:42 +0800

    feat:change signin page title (#221)
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- components/signin/index.tsx
+++ components/signin/index.tsx
@@ -181,7 +181,7 @@ export default function SignIn() {
               <Typography variant="h4" gutterBottom>
                 Welcome back!
               </Typography>
-              <Typography variant="subtitle1" gutterBottom fontFamily="mabry-light,sans-serif">
+              <Typography variant="body1" gutterBottom fontFamily="mabry-light,sans-serif">
                 Through which you can easily configure clusters and view cluster information.
               </Typography>
             </Box>

f5feccd72c7feb07d93dfed531bbb0ae0a7f9d0e
zhaoxinxin <1186037180@qq.com>
2023-08-21 18:21:53 +0800

    fix: Modify Max page Size as a constant
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,5 @@ export const ROLE_ROOT = 'root';
 export const ROLE_GUEST = 'guest';
 export const DEFAULT_PAGE_SIZE = 10;
 export const MAX_PAGE_SIZE = 10000000;
-export const DEFAULT_SCHEDULER_PAGE_SIZE = 5;
-export const DEFAULT_SEEDPEER_PAGE_SIZE = 5;
+export const DEFAULT_SCHEDULER_TABLE_PAGE_SIZE = 5;
+export const DEFAULT_SEEDPEER_TABLE_PAGE_SIZE = 5;

31e42e0bd4ed3ab5acb00991fd1b1e946a5f1948
zhaoxinxin <1186037180@qq.com>
2023-10-26 17:15:08 +0800

    feat: code coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config);
-      // include any other plugin code...
 
-      // It's IMPORTANT to return the config object
-      // with any changed environment variables
       return config;
     },
     baseUrl: 'http://fe.alipay.net:3000',

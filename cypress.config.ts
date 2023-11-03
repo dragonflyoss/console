740ae3b6ea699f1186a3717238388d22c81ea3ca
zhaoxinxin <1186037180@qq.com>
2023-11-03 17:59:36 +0800

    feat: cypress text

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     },
     baseUrl: 'http://localhost:3000',
   },
-  requestTimeout: 120000,
-  responseTimeout: 120000,
+  requestTimeout: 10000,
+  pageLoadTimeout: 10000,
+  responseTimeout: 10000,
 });

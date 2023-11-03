5623c9d13b95248e6a23d0cd6f1b99b347f7d24f
zhaoxinxin <1186037180@qq.com>
2023-11-03 15:17:30 +0800

    feat: cypress text

--- cypress/e2e/404.cy.js
+++ cypress/e2e/404.cy.js
@@ -1,11 +1,12 @@
 describe('The 404 Page', () => {
-  it('sets auth cookie when logging in via form submission', function () {
+  it('404', function () {
     // destructuring assignment of the this.currentUser object
     // const { username, password } = this.currentUser;
 
     cy.visit('/404');
 
     cy.get('.MuiButtonBase-root').click();
+
     cy.url().should('include', '/signin');
   });
 });

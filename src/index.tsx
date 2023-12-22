c0ff4a036371992b6d5b2e30dc02388ffcbb3689
zhaoxinxin <1186037180@qq.com>
2023-12-22 15:09:42 +0800

    feat: Modify directory structure
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Routers } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const theme = createTheme({

51567a78a8c354e2a30b9ecc7f8d025af59aea52
zhaoxinxin <1186037180@qq.com>
2024-04-17 19:55:52 +0800

    feat: change page loading function
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- src/components/loading-backdrop.tsx
+++ src/components/loading-backdrop.tsx
@@ -1,22 +1,21 @@
-import { Backdrop, Box } from "@mui/material"
+import { Backdrop, Box } from '@mui/material';
 
 interface LoadingBackdropProps {
-    open: boolean;
+  open: boolean;
 }
 
 const LoadingBackdrop: React.FC<LoadingBackdropProps> = ({ open }) => {
+  return (
+    <Backdrop
+      open={open}
+      sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        backgroundColor: 'rgba(0,0,0,0.3)',
+      }}
+    >
+      <Box component="img" sx={{ width: '4rem', height: '4rem' }} src="/icons/cluster/page-loading.svg" />
+    </Backdrop>
+  );
+};
 
-    return (
-        <Backdrop
-            open={open}
-            sx={{
-                zIndex: (theme) => theme.zIndex.drawer + 1,
-                backgroundColor: 'rgba(0,0,0,0.3)',
-            }}
-        >
-            <Box component="img" sx={{ width: '4rem', height: '4rem' }} src="/icons/cluster/page-loading.svg" />
-        </Backdrop>
-    )
-}
-
-export default LoadingBackdrop
+export default LoadingBackdrop;

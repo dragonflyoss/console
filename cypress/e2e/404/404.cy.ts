94f6b4134eb4d27b5664cbfd802be24e1ef58502
zhaoxinxin <1186037180@qq.com>
2025-04-28 22:21:33 +0800

    chore: change 404 e2e test
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- cypress/e2e/404/404.cy.ts
+++ cypress/e2e/404/404.cy.ts
@@ -7,14 +7,18 @@ describe('404', () => {
 
   it('show 404 page', () => {
     cy.visit('/root');
+
     cy.get('#404-help-text').should('have.text', `The page you were looking for doesn't exist.`);
   });
 
   it('click the `go back cluster` button', () => {
     cy.visit('/root');
 
+    // Display 404 help text.
     cy.get('#something-went-wrong').should('have.text', 'Something gone wrong!');
     cy.get('#404-help-text').should('have.text', `The page you were looking for doesn't exist.`);
+
+    // Click go back cluster button.
     cy.get('#go-back').click();
 
     // Then I see that the current page is the clusters.

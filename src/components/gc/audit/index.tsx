a32701ada03f1c2d11945455abdbc8988d5bcfd9
zhaoxinxin <1186037180@qq.com>
2025-07-03 18:47:42 +0800

    GC history TTL display error
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- src/components/gc/audit/index.tsx
+++ src/components/gc/audit/index.tsx
@@ -732,7 +732,7 @@ export default function AuditGC() {
                     </TableCell>
                     <TableCell align="center" id={`ttl-${item?.id}`}>
                       <Typography variant="body1" component="div">
-                        {ms(item?.args?.ttl / 100000, { long: true })}
+                        {ms(item?.args?.ttl / 1000000 || 0, { long: true })}
                       </Typography>
                     </TableCell>
                     <TableCell align="center" id={`purged-${item?.port}`}>
